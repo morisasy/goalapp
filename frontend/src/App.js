3a1eea104e2085bfbe495c142b45cd40e40e044e
morisasy <morisasy@gmail.com>
2022-02-15 17:23:43 +0200

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import { Counter } from './features/counter/Counter';
+//import './App.css';
+// npm run client 
+const App = () => {
+  return (
+    <div>
+      <h1>My App</h1>
+    </div>
+  )
+}
+
+export default App;
